531e3076bc76cf071255a31d38107facfc7db4a3
khushichauhan-26 <chauhankhushi586@gmail.com>
2025-05-21 10:54:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+import Navbar from './navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to the Job Board!</h1>
+      <p>This is the beginning of your React journey.</p>
+    </div>
+  );
+}
+
+export default App;
+
