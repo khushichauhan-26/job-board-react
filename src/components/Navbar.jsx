0e3a96e3954711e8169380afc5f8c6ca0adb27ed
khushichauhan-26 <chauhankhushi586@gmail.com>
2025-05-21 16:21:01 +0530

    Day 2/3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css'
 function Navbar() {
     return (
         <nav className="navbar">
-            <h1>Job Board</h1>
+            <h1>Jobify</h1>
             <ul className="links">
                 <li><a href="/home">Home</a></li>
                 <li><a href="/create">New Job</a></li>
