2d120d84f76084d9ffa089a9b675075c9a27c5f0
khushichauhan-26 <chauhankhushi586@gmail.com>
2025-06-10 15:21:41 +0530

    frontend done

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -6,7 +6,7 @@ import { DashboardContext } from './dashboardContext'; // <-- import context
 
 const Dashboard = ({ isDarkThemeEnabled }) => {
   // temp
-  const user = { name: 'Aadi' };
+  const user = { name: 'Khushi' };
   const [showSidebar, setShowSidebar] = useState(false);
   const [isDarkTheme, setIsDarkTheme] = useState(isDarkThemeEnabled);
   const toggleDarkTheme = () => {
